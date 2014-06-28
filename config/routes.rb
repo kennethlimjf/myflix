70cf929452f2221534c19c8f49bb30d6d31ffe44
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-29 01:26:08 +0800

    Implemented search by title functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get 'home', to: 'main#home'
-  resources :videos, only: :show
+  resources :videos, only: :show do
+    get 'search', to: 'videos#search', on: :collection
+  end
   resources :categories, only: :show
 end
