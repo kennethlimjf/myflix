c8d7da268c1be986c1659cbd8fd27648d1907453
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-30 00:56:38 +0800

    Remove some line breaks

--- app/helpers/register_form_builder.rb
+++ app/helpers/register_form_builder.rb
@@ -2,10 +2,8 @@ class RegisterFormBuilder < ActionView::Helpers::FormBuilder
 
   def form_group_tag(method, &block)
     form_class = "form-group"
-    
     errors = object.errors[method.to_sym]
     form_class += " has-error" if errors.any?
-
     @template.div_for(object, class: "#{form_class}", &block)
   end
 
