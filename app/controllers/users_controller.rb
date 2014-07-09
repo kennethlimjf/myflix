3b9adbebff79a7daaba62c1dec3aa8b082038b6b
Kenneth Lim <kennethlimjf@gmail.com>
2014-07-09 17:19:56 +0800

    Added error handling for sending email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,11 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      UserMailer.register_user(@user).deliver
+      begin
+        UserMailer.register_user(@user).deliver
+      rescue Net::SMTPAuthenticationError
+        flash[:error] = "Account created, however there is a problem with sending welcome email."
+      end
       flash[:notice] = "Your new account has been created."
       redirect_to root_path
     else
