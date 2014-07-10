2e167ba7b9fe6cb9370c6682fad2e5704544d1fc
Kenneth Lim <kennethlimjf@gmail.com>
2014-07-10 18:39:54 +0800

    Updated to mailgun for actionmailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,14 +18,13 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'immense-basin-1515.herokuapp.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'immense-basin-1515.heroku.com',
+    :authentication => :plain,
   }
+  config.action_mailer.delivery_method = :smtp
 end
