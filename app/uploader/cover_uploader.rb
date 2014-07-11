a3d0d5b265c016bbaa7b55417004d2b6b6bc68de
Kenneth Lim <kennethlimjf@gmail.com>
2014-07-11 17:26:36 +0800

    Updated fog and carrierwave settings. updated spec test for uploader

--- app/uploader/cover_uploader.rb
+++ app/uploader/cover_uploader.rb
@@ -1,7 +1,5 @@
 class CoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  
-  storage :fog
 
   process :resize_to_fit => [664, 375]
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,17 @@ require 'fog/aws/storage'
 require 'carrierwave'
 
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET']
-  }
-
-  config.fog_directory =  ENV['S3_BUCKET_NAME']
-  config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
+  if Rails.env.staging? || Rails.env.production? || Rails.env.development?
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+      :region                 => 'ap-southeast-1',
+      :host                   => Rails.env.production? ? 'movix.herokuapp.com' : 'movix-staging.herokuapp.com'
+    }
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
+  else
+    config.storage :file
+    config.enable_processing = Rails.env.development?
+  end
 end
